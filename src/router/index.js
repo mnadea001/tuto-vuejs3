3c1259d282cc1b5d7ebb4de0b1ec20773b92e161
mnadea001 <mathilde.nadeau@hotmail.fr>
2023-09-25 16:52:35 +0200

    init project

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import EventList from "../views/EventList.vue";
+import AboutView from "../views/AboutView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "EventList",
+    component: EventList,
+  },
+  {
+    path: "/about",
+    name: "AboutView",
+    component: AboutView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/EventList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: "animal welfare",
+          title: "Cat adoption Day",
+          description: "Find your new feline friend at this event.",
+          location: "Meow Town",
+          date: "January 28, 2024",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "Kat Laydee",
+        },
+        {
+          id: 5963101,
+          category: "animal protection",
+          title: "Cat protection Day",
+          description: "Protect your new feline friend at this event.",
+          location: "Cat Town",
+          date: "March 15, 2024",
+          time: "18:00",
+          petsAllowed: true,
+          organizer: "Max Cat",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
